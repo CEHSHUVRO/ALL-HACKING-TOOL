5da87badf54ae20eeea64109530205dcab71aa8d
SHUVRO KARMAKAR <167098536+CEHSHUVRO@users.noreply.github.com>
2024-12-26 20:16:44 +0530

    Update ALL-HACKING-TOOL.sh

--- ALL-HACKING-TOOL.sh
+++ ALL-HACKING-TOOL.sh
@@ -10,7 +10,7 @@ echo -e '\033[31;40;1m
  ╚═╝  ╚═╝╚══════╝           ╚═╝    ╚═════╝  ╚═════╝ ╚══════╝ V5s
   Coded by MR.SHUVRO
   github: https://github.com/CEHSHUVRO
-\033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m 4lbH4cker
+\033[33;4mVersion:\033[0m 4            \033[33;4mCTRL+C:\033[0m exit          \033[33;4mAuthor:\033[0m CEHSHUVRO
 
 \e[37m[1]\e[36m Kërkesat & Përditësimi        \e[37m[2]\e[36m Phishing Tool				
 \e[37m[3]\e[36m WebCam Hack                   \e[37m[4]\e[36m Subscan			
